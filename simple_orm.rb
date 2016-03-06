578965333b52e03c0b3bb4464e4bab9437fee609
Wasim Akram <mailwasim@gmail.com>
2016-03-06 22:34:49 +0400

    Added column_names accessors for to_s methods

--- simple_orm.rb
+++ simple_orm.rb
@@ -3,6 +3,7 @@ require "pry"
 
 module SimpleOrm
 
+  ## Query Methods ##
   def all(options={})
     result = conn.query("SELECT * FROM #{table_name}")
     objs_array = []
@@ -25,34 +26,25 @@ module SimpleOrm
     end
   end
 
-  def conn
-    @conn ||= get_conn
+  ## Converters ##
+  def to_s
+    to_h.to_s
   end
 
-  private
-
-  def init_for_orm(row)
-    new_object = self.class.new
-    row.each do |key, value|
-      # TODO : check only once
-      define_instance_variables_for_class unless new_object.respond_to?("#{key}=")
-      new_object.send("#{key}=", value)
+  def to_h
+    hash = {}
+    @column_names.each do |column_name|
+      hash[column_name.to_sym] = self.send(column_name)
     end
-    return new_object
+    hash.to_s
   end
 
-  def define_instance_variables_for_class
-    # TODO : SHOW COLUMNS FROM users; row has Field with column name
-    result = conn.query("SELECT * FROM #{table_name}")
-    row = result.first
+  def column_names
+    @column_names
+  end
 
-    klass = self.class
-    row.keys.each do |key|
-      klass.class_eval do
-        define_method(key) { klass.instance_variable_get("@#{key}"); }
-        define_method("#{key}=") { |attr_value| klass.instance_variable_set("@#{key}", attr_value); }
-      end
-    end
+  def column_names=(cns)
+    @column_names = cns
   end
 
   def table_name
@@ -63,15 +55,47 @@ module SimpleOrm
     @table_name = tb_name
   end
 
+  private
+
+  def conn
+    @conn ||= get_conn
+  end
+
+  def init_for_orm(row)
+    new_object = self.class.new
+    row.each do |key, value|
+      # TODO : check only once
+      define_instance_variables_for_class(new_object) unless new_object.respond_to?("#{key}=")
+      new_object.send("#{key}=", value)
+    end
+    return new_object
+  end
+
+  def define_instance_variables_for_class(new_object)
+    # TODO : SHOW COLUMNS FROM users; row has Field with column name
+    result = conn.query("SELECT * FROM #{table_name}")
+    row = result.first
+
+    klass = self.class
+    new_object.column_names = row.keys
+    new_object.column_names.each do |key|
+      klass.class_eval do
+        define_method(key) { klass.instance_variable_get("@#{key}"); }
+        define_method("#{key}=") { |attr_value| klass.instance_variable_set("@#{key}", attr_value); }
+      end
+    end
+  end
+
+
   def get_conn
-    @db_host  = "127.0.0.1"
-    @db_user  = "root"
-    @db_password  = ""
-    @db_name = "organizations"
-    @client = Mysql2::Client.new(
-      host: @db_host,
-      username: @db_user,
-      password: @db_password,
-      database: @db_name)
+    db_host  = "127.0.0.1"
+    db_user  = "root"
+    db_password  = ""
+    db_name = "organizations"
+    client = Mysql2::Client.new(
+      host: db_host,
+      username: db_user,
+      password: db_password,
+      database: db_name)
   end
 end
--- spec/models/simple_orm_spec.rb
+++ spec/models/simple_orm_spec.rb
@@ -10,33 +10,52 @@ end
 describe SimpleOrm do
   describe User do
 
-    subject {User.new}
+    let(:user) {User.new}
 
     describe "#all" do
+      subject {user.all}
       it "should be able to call all" do
-        expect(subject).to respond_to(:all)
+        expect(user).to respond_to(:all)
       end
 
       it "should return an array" do
-        expect(subject.all.class).to eq(Array)
+        expect(subject.class).to eq(Array)
       end
     end
 
     describe "#find" do
+      subject { user.find(1) }
+
       it "should be able to call find" do
-        expect(subject).to respond_to(:find)
+        expect(user).to respond_to(:find)
       end
 
       it "should return the mixin class's object" do
-        expect(subject.find(1).class).to eq(User)
+        expect(subject.class).to eq(User)
       end
 
       it "should have data for user" do
         # TODO : populate test data
-        expect(subject.find(1).id).to eq(1)
-        expect(subject.find(1).user_type).to eq("SUPER ADMIN")
-        expect(subject.find(1).username).to eq("admin")
-        expect(subject.find(1).password).to eq("admin")
+        expect(subject.id).to eq(1)
+        expect(subject.user_type).to eq("SUPER ADMIN")
+        expect(subject.username).to eq("admin")
+        expect(subject.password).to eq("admin")
+      end
+    end
+
+    describe "#column_names" do
+      subject { user.column_names }
+      it "should be able to call column_names" do
+        expect(user).to respond_to(:column_names)
+      end
+
+      it "should return the Array of column names" do
+        expect(subject.class).to eq(Array)
+      end
+
+      it "should have table column names" do
+        # TODO : populate test data
+        expect(subject).to eq([])
       end
     end
   end
