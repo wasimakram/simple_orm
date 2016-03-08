82854c171cb125cdb3b216bef3b2d49db8ea4c7b
Wasim Akram <mailwasim@gmail.com>
2016-03-08 10:02:35 +0400

    Moved to expectation syntax of rspec

--- spec/models/simple_orm_spec.rb
+++ spec/models/simple_orm_spec.rb
@@ -35,17 +35,40 @@ describe SimpleOrm do
       end
     end
 
+    describe ".find" do
+      subject { test_class.find(1) }
+      it "be able to call find" do
+        expect(test_class).to respond_to(:find)
+      end
+
+      it "be able to call find" do
+        expect(test_class).to respond_to(:find)
+      end
+
+      it "return the mixin class's object" do
+        expect(subject.class).to eq(test_class)
+      end
+
+      it "have data for user" do
+        # TODO : populate test data using fixtures
+        expect(subject.id).to eq(1)
+        expect(subject.user_type).to eq("SUPER ADMIN")
+        expect(subject.username).to eq("admin")
+        expect(subject.password).to eq("admin")
+      end
+    end
+
     describe ".all" do
       subject { test_class.all }
-      it "should be able to call all" do
+      it "be able to call all" do
         expect(test_class).to respond_to(:all)
       end
 
-      it "should return an array" do
+      it "return an array" do
         expect(subject.class).to eq(Array)
       end
 
-      it "should return a list of all users" do
+      it "return a list of all users" do
         test_array = []
         (1..7).each do |index|
           test_array << test_class.find(index).to_h
@@ -55,44 +78,25 @@ describe SimpleOrm do
     end
 
     describe ".first" do
-      xit "should be able to call first"
+      xit "be able to call first"
     end
 
     describe ".order" do
-      xit "should be able to call order"
+      xit "be able to call order"
     end
 
-    describe ".find" do
-      subject { test_class.find(1) }
-
-      it "should be able to call find" do
-        expect(test_class).to respond_to(:find)
-      end
-
-      it "should return the mixin class's object" do
-        expect(subject.class).to eq(test_class)
-      end
-
-      it "should have data for user" do
-        # TODO : populate test data using fixtures
-        expect(subject.id).to eq(1)
-        expect(subject.user_type).to eq("SUPER ADMIN")
-        expect(subject.username).to eq("admin")
-        expect(subject.password).to eq("admin")
-      end
-    end
 
     describe ".column_names" do
       subject { test_class.column_names }
-      it "should be able to call column_names" do
+      it "be able to call column_names" do
         expect(test_class).to respond_to(:column_names)
       end
 
-      it "should return the Array of column names" do
+      it "return the Array of column names" do
         expect(subject.class).to eq(Array)
       end
 
-      it "should have table column names" do
+      it "have table column names" do
         expect(subject).to eq(test_column_names)
       end
     end
@@ -104,22 +108,22 @@ describe SimpleOrm do
     subject { Employee }
 
     describe "#all" do
-      it "should be able to call all" do
+      it "be able to call all" do
         expect(subject).to respond_to(:all)
       end
 
-      it "should return an array" do
+      it "return an array" do
         expect(subject.all.class).to eq(Array)
       end
 
     end
 
     describe "#find" do
-      it "should be able to call find" do
+      it "be able to call find" do
         expect(subject).to respond_to(:find)
       end
 
-      it "should return  the mixedin class's object" do
+      it "return  the mixedin class's object" do
         expect(subject.find(1).class).to eq(Employee)
       end
     end
